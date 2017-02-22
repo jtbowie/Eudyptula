9eacf2edbd16136baca30ee985963c8272fe2c15
Jonathan Bowie <eudjtb@gmail.com>
2017-02-22 14:36:43 -0800

    cleaned up module Makefiles

--- task4/mod_hello_girls.c
+++ task4/mod_hello_girls.c
@@ -1,16 +1,20 @@
-#include <linux/module.h> /* We're a module */
-#include <linux/kernel.h> /* Alex the Kidd in Kernel-Land */
+#include <linux/module.h> 
+#include <linux/kernel.h> 
 
-/* Init our module. Constructor Power! */
+/* 
+ * Init our module. Constructor Power! 
+ */
 
 int init_module(void)
 {
-	printk(KERN_DEBUG "Hello World!\n"); /* Let the world know it */
+	printk(KERN_DEBUG "Hello World!\n"); 
 
-	return(0); /* Insmod will fail without return. */
+	return(0);
 }
 
-/* All good things must come to an end */
+/* 
+ * All good things must come to an end 
+ */
 
 void cleanup_module(void)
 {
