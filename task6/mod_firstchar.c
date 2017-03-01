22682d51d4c4ebfb697f36e7a45ff1983d04d0f8
Jonathan Bowie <eudjtb@gmail.com>
2017-02-28 23:57:29 -0800

    prettied up mod_firstchar, added local header

--- task6/mod_firstchar.c
+++ task6/mod_firstchar.c
@@ -1,46 +1,56 @@
 #include <linux/init.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
-#include <linux/module.h> /* We're a module */
-#include <linux/kernel.h> /* Alex the Kidd in Kernel-Land */
+#include <linux/module.h>
+#include <linux/kernel.h>
 #include <asm/uaccess.h>
+#include "firstchar.h"
 
-#define MY_DEV_NAME "firstchar"
+#define MY_DEV_NAME "eudyptula"
 #define MAX_MSG_LEN 0xFF
 
 static int occupied;
 static struct miscdevice firstchar_dev;
-static char msg[MAX_MSG_LEN];
-static char *msgptr;
+static char output[MAX_MSG_LEN];
 
 static int firstchar_open(struct inode *inode, struct file *filp)
 {
-	static int counter = 0;
+	static int counter;
+
 	if (occupied > 0)
 		return -EBUSY;
-	sprintf(msg, "This device has been opened %d times!\n", counter++);
+
+	sprintf(output, "This device has been opened %d times!\n", counter++);
 	occupied++;
-	msgptr = (char *)&msg;
 	try_module_get(THIS_MODULE);
 	return 0;
 }
 
-static ssize_t firstchar_read(struct file *filp, char __user *buffer, size_t len, loff_t *offset)
+static ssize_t firstchar_read(struct file *filp, char __user *buffer,
+				size_t len, loff_t *offset)
 {
-	int consumed = 0;
 
-	while (len && *msgptr) {
-		put_user(*(msgptr++),buffer++);
-		len--;
-		consumed++;
-	}
+/*
+ * Never write more than strlen(output) bytes.  If len < strlen(output)
+ * write only len bytes.
+ */
+	size_t my_len = len < strlen(output) ? len : strlen(output);
 
-	return consumed;
+	if (*offset > 0)
+		return 0;
+
+	if (copy_to_user(buffer, (char *)&output, my_len))
+		return -EINVAL;
+
+	*offset += my_len;
+
+	return my_len;
 }
 
-static ssize_t firstchar_write(struct file *filp, const char __user *usr, size_t len, loff_t *offset)
+static ssize_t firstchar_write(struct file *filp, const char __user *usr,
+				size_t len, loff_t *offset)
 {
-	printk(KERN_ALERT "Unauthorized write to /dev/%s\n", MY_DEV_NAME);
+	pr_alert("Unauthorized write to /dev/%s\n", MY_DEV_NAME);
 	return -EINVAL;
 }
 
@@ -52,7 +62,7 @@ static int firstchar_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static struct file_operations fops = {
+static const struct file_operations fops = {
 	.read = firstchar_read,
 	.open = firstchar_open,
 	.write = firstchar_write,
@@ -62,7 +72,7 @@ static struct file_operations fops = {
 static int __init firstchar_init(void)
 {
 	static int ret;
-	
+
 	firstchar_dev.minor = MISC_DYNAMIC_MINOR;
 	firstchar_dev.name = MY_DEV_NAME;
 	firstchar_dev.fops = &fops;
@@ -71,17 +81,18 @@ static int __init firstchar_init(void)
 	if (ret)
 		return ret;
 
-	printk(KERN_DEBUG "Loaded %s successfully!\n", MY_DEV_NAME); /* Let the world know it */
-	printk(KERN_DEBUG "%s got minor number %d\n", MY_DEV_NAME, firstchar_dev.minor);
+	pr_notice("Loaded %s successfully!\n", MY_DEV_NAME);
+	pr_notice("%s got minor number %d\n", MY_DEV_NAME,
+		firstchar_dev.minor);
 
-	return(0); /* Insmod will fail without return. */
+	return 0;
 }
 
 /* All good things must come to an end */
 
 void cleanup_module(void)
 {
-	printk(KERN_DEBUG "I hate to see you leave, but...\n");
+	pr_notice("I hate to see you leave, but...\n");
 	misc_deregister(&firstchar_dev);
 }
 
