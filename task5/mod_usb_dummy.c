2789d7e5f2e7e29ad97f5586e662ff18c8513c8c
Jonathan Bowie <eudjtb@gmail.com>
2017-02-28 22:14:22 -0800

    Removed unnecessary USB driver register code

--- task5/mod_usb_dummy.c
+++ task5/mod_usb_dummy.c
@@ -4,7 +4,6 @@
 #include <linux/usb.h>
 #include <linux/usb/input.h>
 #include <linux/hid.h>
-#include "usb_dummy.h"
 
 /*
  * Use USB_INTERFACE_INFO macro to populate our usb_device "object".
@@ -17,13 +16,6 @@ static struct usb_device_id info_table [] = {
 	{},
 };
 
-static struct usb_driver usb_dummy_hid_drv = {
-	.name =		"usb_dummy_hid",
-	.probe =	usb_dummy_hid_probe,
-	.disconnect = 	usb_dummy_hid_disconnect,
-	.id_table =	info_table,
-};
-
 /*
  * Add ourselves to the usb device list.
  */
@@ -34,28 +26,11 @@ MODULE_DEVICE_TABLE(usb,info_table);
  * These will never be called.  Sad panda is sad.
  */
 
-static int usb_dummy_hid_probe(struct usb_interface *intf, 
-		const struct usb_device_id *id)
-{
-	pr_alert("Found a Keyboard!  Hello World!\n");
-	return 0;
-}
-
-static void usb_dummy_hid_disconnect(struct usb_interface *intf)
-{
-	pr_alert("Hey!@@#$ Come back here :(\n");
-}
-
 int usb_dummy_hid_init(void)
 {
-	int ret;
-
 	pr_debug("Loading dummy HID(Keyboard) module\n"); 
-	ret = usb_register(&usb_dummy_hid_drv);
-	if (ret)
-		pr_warn("usb_register failed.  Error number %d", ret);
 
-	return ret;
+	return 0;
 }
 
 /* 
@@ -65,7 +40,6 @@ int usb_dummy_hid_init(void)
 void usb_dummy_hid_cleanup(void)
 {
 	pr_debug("I hate to see you leave, but...\n");
-	usb_deregister(&usb_dummy_hid_drv);
 }
 
 module_init(usb_dummy_hid_init);
--- task5/usb_dummy.h
+++
@@ -1,8 +0,0 @@
-#ifndef USB_DUMMY_H
-#define USB_DUMMY_H
-
-static int usb_dummy_hid_probe(struct usb_interface *, 
-			const struct usb_device_id *); 
-static void usb_dummy_hid_disconnect(struct usb_interface *);
-
-#endif
